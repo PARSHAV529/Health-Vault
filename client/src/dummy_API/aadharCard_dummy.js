daf071a5d85ab2cd0d47887598b0c084e78d6dc4
Parshav <parshav143@gmail.com>
2024-02-23 23:35:57 +0530

    api commit

---
+++ client/src/dummy_API/aadharCard_dummy.js
@@ -0,0 +1,85 @@
+const aadhar = [
+    {
+        aadharNumber:123456789801,
+        userName:'Parshav Andhariya',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber: '432156789801',
+        userName:'Parthiv Chaudhari',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber:123478569801,
+        userName:'Raj Acharya',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber:123456789867,
+        userName:'Maulik Chaudhari',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber:123489759801,
+        userName:'Nishan Chaudhari',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber:657856789801,
+        userName:'Abhi Patel',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber:123474129801,
+        userName:'Raj Ajwaliya',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber:369856789801,
+        userName:'Harsh Ajwaliya',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber:891056789801,
+        userName:'Dharmik Ajwaliya',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+    {
+        aadharNumber:123456784587,
+        userName:'Dhrumit Patel',
+        userDOB:'20/08/2004',
+        userConatctNumber:7894563579,
+        userAddress:'bhavnagar' 
+
+    },
+
+]
+
+export default aadhar;
