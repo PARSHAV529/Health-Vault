5615e44bfa69d5cf34225580fe46e7236d3d6591
Parshav <parshav143@gmail.com>
2024-02-24 03:07:53 +0530

    ui2 commit

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,28 +1,35 @@
 // Import necessary dependencies
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 
 // Import your components
 import LogIn from '../Pages/Login/Login';
 import RegisterForm from '../Pages/Signup/SignUp';
 import Home from '../Pages/Home';
+import DrawerAppBar from './Header';
 
 // Define your routes
 const Navigation = () => {
   return (
-    <Router>
-      <div>
-        {/* Set up the Switch component to handle routing */}
-        <Routes>
-          {/* Define individual routes using the Route component */}
-          <Route exact path="/" element={Home} />
-          <Route path="/login" element={LogIn} />
-          <Route path="/signup" element={RegisterForm} />
-          {/* Handle 404 Not Found */}
-          {/* <Route component={NotFound} /> */}
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter >
+<DrawerAppBar/>
+      <Routes>
+        <Route path="/signup" element={<RegisterForm />} />
+        <Route path="/" element={<Home  />} />
+        <Route path="/login" element={ <  LogIn/>} />
+       
+
+
+
+
+      </Routes>
+
+
+
+
+
+
+    </BrowserRouter>
   );
 };
 
